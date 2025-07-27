b904bea61be5703f1475d6db9dc659f2a00e3ca6
Rivindu-Kowlinda <rivindukowlinda2@gmail.com>
2025-07-27 11:10:17 +0530

    test

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -3,39 +3,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-export interface EmployeeStat {
-  username: string;
-  pointsEarned: number;
-  helpsProvided: number;
-}
-
-export interface Points {
-  earned: number;
-  spent: number;
-  monthlyAllocation: number;
-  balance: number;
-}
-
-export interface RequestMade {
-  id: string;
-  requestName: string;
-  status: string;
-  cost: number;
-  createdAt: string;
-}
-
-export interface DashboardResponse {
-  employeeStats: EmployeeStat[];
-  points: Points;
-  requestsMade: RequestMade[];
-  helpsProvided: RequestMade[];  // same shape as requestsMade
-}
-
 @Injectable({ providedIn: 'root' })
 export class DashboardService {
-  private url = 'http://localhost:8080/api/employee/dashboard';
+  private dashboardUrl = 'http://localhost:8080/api/dashboard';
+
   constructor(private http: HttpClient) {}
-  getDashboard(): Observable<DashboardResponse> {
-    return this.http.get<DashboardResponse>(this.url);
+
+  getDashboardData(): Observable<any> {
+    return this.http.get<any>(this.dashboardUrl);
   }
 }
